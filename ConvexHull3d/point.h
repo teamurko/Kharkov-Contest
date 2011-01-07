4d8dd3a663fb36fb16441b6d05991d097543db42
Stanislav Pak <abss.pakso@gmail.com>
2011-01-07 21:17:15 +0300

    added tests

--- ConvexHull3d/point.h
+++ ConvexHull3d/point.h
@@ -1,6 +1,7 @@
 #ifndef POINT_H_INCLUDED
 #define POINT_H_INCLUDED
 
+#include "utils.h"
 #include <vector>
 #include <cmath>
 #include <iostream>
@@ -13,10 +14,10 @@ double det(double a, double b, double c, double d)
 class Point
 {
 public:
-    Point() : x_(0), y_(0), z_(0) {}
+    Point() : x_(0), y_(0), z_(0), id_(0) {}
 
     Point(double x, double y, double z) :
-        x_(x), y_(y), z_(z) {}
+        x_(x), y_(y), z_(z), id_(0) {}
     
     double x() const { return x_; }
 
@@ -36,13 +37,23 @@ public:
 
     size_t id() const { return id_; }
 
+    void setId(size_t id) { id_ = id; }
+
     friend std::istream& operator>>(std::istream& in, Point& point); 
+    friend std::ostream& operator<<(std::ostream& out, const Point& point);
 private:
     double x_;
     double y_;
     double z_;
     size_t id_;
 };
+
+std::ostream& operator<<(std::ostream& out, const Point& point)
+{
+    out.setf(std::ios::fixed, std::ios::floatfield);  
+    out.precision(10);
+    out << point.x() << " " << point.y() << " " << point.z();
+}
 
 Point operator-(const Point& pointA, const Point& pointB)
 {
@@ -70,6 +81,14 @@ bool operator<(const Point& pointA, const Point& pointB)
         return pointA.y() < pointB.y();
     }
     return pointA.z() < pointB.z();
+}
+
+bool operator==(const Point& pointA, const Point& pointB)
+{
+    return pointA.id() == pointB.id() &&
+        fabs(pointA.x() - pointB.x()) < constants::EPS &&
+        fabs(pointA.y() - pointB.y()) < constants::EPS &&
+        fabs(pointA.z() - pointB.z()) < constants::EPS;
 }
 
 Point vectorProduct(const Point& a, const Point& b)
